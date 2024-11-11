2f9bdd80b15fa4f61d3afc26cf64846f90895c6e
start.vaadin.com <no-reply@vaadin.com>
2024-11-11 23:56:35 +0000

    Generated project

---
+++ src/main/java/com/educontrol/application/views/modulodeprofesor/ModulodeProfesorView.java
@@ -0,0 +1,81 @@
+package com.educontrol.application.views.modulodeprofesor;
+
+import com.vaadin.flow.component.Composite;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.datepicker.DatePicker;
+import com.vaadin.flow.component.formlayout.FormLayout;
+import com.vaadin.flow.component.html.H3;
+import com.vaadin.flow.component.orderedlayout.FlexComponent.Alignment;
+import com.vaadin.flow.component.orderedlayout.FlexComponent.JustifyContentMode;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.EmailField;
+import com.vaadin.flow.component.textfield.TextArea;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.router.Menu;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.theme.lumo.LumoUtility.Gap;
+
+@PageTitle("Modulo de Profesor")
+@Route("person-form")
+@Menu(order = 1, icon = "line-awesome/svg/user.svg")
+public class ModulodeProfesorView extends Composite<VerticalLayout> {
+
+    public ModulodeProfesorView() {
+        VerticalLayout layoutColumn2 = new VerticalLayout();
+        H3 h3 = new H3();
+        FormLayout formLayout2Col = new FormLayout();
+        TextField textField = new TextField();
+        TextField textField2 = new TextField();
+        DatePicker datePicker = new DatePicker();
+        EmailField emailField = new EmailField();
+        TextField textField3 = new TextField();
+        TextField textField4 = new TextField();
+        TextArea textArea = new TextArea();
+        HorizontalLayout layoutRow = new HorizontalLayout();
+        Button buttonPrimary = new Button();
+        Button buttonSecondary = new Button();
+        getContent().setWidth("100%");
+        getContent().getStyle().set("flex-grow", "1");
+        getContent().setJustifyContentMode(JustifyContentMode.START);
+        getContent().setAlignItems(Alignment.CENTER);
+        layoutColumn2.setWidth("100%");
+        layoutColumn2.setMaxWidth("800px");
+        layoutColumn2.setHeight("min-content");
+        h3.setText("Personal Information");
+        h3.setWidth("100%");
+        formLayout2Col.setWidth("100%");
+        textField.setLabel("Nombre");
+        textField2.setLabel("Apellido");
+        datePicker.setLabel("Fecha de nacimiento");
+        emailField.setLabel("Email");
+        emailField.setWidth("min-content");
+        textField3.setLabel("Número de identificación");
+        textField4.setLabel("Especialidad");
+        textArea.setLabel("Dirección");
+        textArea.setWidth("100%");
+        layoutRow.addClassName(Gap.MEDIUM);
+        layoutRow.setWidth("100%");
+        layoutRow.getStyle().set("flex-grow", "1");
+        buttonPrimary.setText("Save");
+        buttonPrimary.setWidth("min-content");
+        buttonPrimary.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+        buttonSecondary.setText("Cancel");
+        buttonSecondary.setWidth("min-content");
+        getContent().add(layoutColumn2);
+        layoutColumn2.add(h3);
+        layoutColumn2.add(formLayout2Col);
+        formLayout2Col.add(textField);
+        formLayout2Col.add(textField2);
+        formLayout2Col.add(datePicker);
+        formLayout2Col.add(emailField);
+        formLayout2Col.add(textField3);
+        formLayout2Col.add(textField4);
+        formLayout2Col.add(textArea);
+        layoutColumn2.add(layoutRow);
+        layoutRow.add(buttonPrimary);
+        layoutRow.add(buttonSecondary);
+    }
+}
---
+++ src/main/java/com/educontrol/application/views/modulohorario/ModuloHorarioView.java
@@ -0,0 +1,63 @@
+package com.educontrol.application.views.modulohorario;
+
+import com.vaadin.flow.component.Composite;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.datepicker.DatePicker;
+import com.vaadin.flow.component.formlayout.FormLayout;
+import com.vaadin.flow.component.html.H3;
+import com.vaadin.flow.component.orderedlayout.FlexComponent.Alignment;
+import com.vaadin.flow.component.orderedlayout.FlexComponent.JustifyContentMode;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.timepicker.TimePicker;
+import com.vaadin.flow.router.Menu;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.theme.lumo.LumoUtility.Gap;
+
+@PageTitle("Modulo Horario")
+@Route("person-form4")
+@Menu(order = 4, icon = "line-awesome/svg/user.svg")
+public class ModuloHorarioView extends Composite<VerticalLayout> {
+
+    public ModuloHorarioView() {
+        VerticalLayout layoutColumn2 = new VerticalLayout();
+        H3 h3 = new H3();
+        FormLayout formLayout2Col = new FormLayout();
+        DatePicker datePicker = new DatePicker();
+        TimePicker timePicker = new TimePicker();
+        HorizontalLayout layoutRow = new HorizontalLayout();
+        Button buttonPrimary = new Button();
+        Button buttonSecondary = new Button();
+        getContent().setWidth("100%");
+        getContent().getStyle().set("flex-grow", "1");
+        getContent().setJustifyContentMode(JustifyContentMode.START);
+        getContent().setAlignItems(Alignment.CENTER);
+        layoutColumn2.setWidth("100%");
+        layoutColumn2.setMaxWidth("800px");
+        layoutColumn2.setHeight("min-content");
+        h3.setText("Personal Information");
+        h3.setWidth("100%");
+        formLayout2Col.setWidth("100%");
+        datePicker.setLabel("Dia");
+        timePicker.setLabel("Hora");
+        timePicker.setWidth("min-content");
+        layoutRow.addClassName(Gap.MEDIUM);
+        layoutRow.setWidth("100%");
+        layoutRow.getStyle().set("flex-grow", "1");
+        buttonPrimary.setText("Save");
+        buttonPrimary.setWidth("min-content");
+        buttonPrimary.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+        buttonSecondary.setText("Cancel");
+        buttonSecondary.setWidth("min-content");
+        getContent().add(layoutColumn2);
+        layoutColumn2.add(h3);
+        layoutColumn2.add(formLayout2Col);
+        formLayout2Col.add(datePicker);
+        formLayout2Col.add(timePicker);
+        layoutColumn2.add(layoutRow);
+        layoutRow.add(buttonPrimary);
+        layoutRow.add(buttonSecondary);
+    }
+}
